02bd53f253e4839d8b28ce83917047ec0c0695b9
guido.pollini <jpollini@hotmail.it>
2016-08-12 12:00:05 +0200

    Started to work on "MayaObserver"

---
+++ API_callbackManager.py
@@ -0,0 +1,26 @@
+import maya.cmds     as MC
+import maya.OpenMaya as OM
+"""
+<PyObject>
+All object types are extensions of this type. This is a type which contains the information Python needs to treat a pointer to an object as an object. 
+(it contains only the object’s reference count and a pointer to the corresponding type object)
+
+
+OM.MSceneMessage.addCallback(...) returns  a <PyCObject> (<MCallbackid> in C++)
+OM.MMessage.removeCallback(...)   requires a <PyCObject> (<MCallbackid> in C++)
+
+<PyCObject>
+This subtype of PyObject represents an opaque value, useful for C extension modules who need to pass an opaque value 
+(as a void* pointer) through Python code to other C code. It is often used to make a C function pointer defined in one
+ module available to other modules, so the regular import mechanism can be used to access C APIs defined in dynamically loaded modules.
+"""
+
+def annihilateTurtle_callback(*args):
+    result = '[CALLBACK] "kBeforeNew"'
+    if MC.pluginInfo('Turtle', query=True, loaded=True):
+        MC.unloadPlugin('Turtle', force=True)
+        result += ': Turtle annihilated:)'
+    print result    
+
+id = OM.MSceneMessage.addCallback(OM.MSceneMessage.kBeforeNew, annihilateTurtle_callback)
+OM.MMessage.removeCallback(id)
